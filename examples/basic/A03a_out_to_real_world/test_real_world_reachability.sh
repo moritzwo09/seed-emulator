b7e3ae8f77f95cced788982d83e67db3a6d3c6d0
Kevin Du <kevin@seed>
2026-06-06 11:04:32 +0800

    finize A03a

--- examples/basic/A03a_out_to_real_world/test_real_world_reachability.sh
+++ examples/basic/A03a_out_to_real_world/test_real_world_reachability.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 docker compose -f output/docker-compose.yml exec -T hnode_151_web \
-    ping -4 -c 3 -W 5 23.192.228.80 >/dev/null
-
-echo "A03a: hnode_151_web can ping 23.192.228.80"
+    ping -4 -c 5 -W 5 23.192.228.80 
